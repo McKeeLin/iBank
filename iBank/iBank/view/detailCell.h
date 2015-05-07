54798879824d9168fd52f450c0953e4304e0d7bb
McKeeLin <mckeelin@icloud.com>
2015-05-07 00:06:12 -0700

    detailVC

---
+++ iBank/iBank/view/detailCell.h
@@ -0,0 +1,13 @@
+//
+//  detailCell.h
+//  iBank
+//
+//  Created by McKee on 15/5/6.
+//  Copyright (c) 2015年 McKee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface detailCell : UITableViewCell
+
+@end
---
+++ iBank/iBank/view/detailCell.m
@@ -0,0 +1,23 @@
+//
+//  detailCell.m
+//  iBank
+//
+//  Created by McKee on 15/5/6.
+//  Copyright (c) 2015年 McKee. All rights reserved.
+//
+
+#import "detailCell.h"
+
+@implementation detailCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
